1c79ba4584e3636310e87f90f7f83f9f36f43dad
Raymond Fang <62193611+2009fang@users.noreply.github.com>
2021-08-31 16:47:02 +0800

    Update

--- Calculator/calculator.h
+++ Calculator/calculator.h
@@ -7,10 +7,12 @@
 #include <vector>
 #include <string>
 #include <sstream>
+#include <corecrt_math_defines.h>
 
 _STD_BEGIN
 
-vector<string> operators({ "+", "-", "*", "/", "^", "(", ")", "sin", "cos", "tan", "log", "log10" });
+vector<string> operators({ "+", "-", "*", "/", "^", "(", ")", "sin", "cos", "tan", "log", "log10", "asin", "acos", "atan", "sqrt", "cubr" });
+vector<string> constants({ "pi", "phi", "e" });
 vector<string> binary_op({ "+", "-", "*", "/", "^" });
 vector<string> empties({ " ", "\t", "\n" });
 
@@ -19,19 +21,10 @@ template <typename Type>
 vector<Type> subvector(vector<Type> vec, size_t start, size_t end)
 {
 	vec.erase(vec.begin(), vec.begin() + start);
-	vec.erase(vec.begin() + end - start, vec.end());
+	if (end - start == vec.size() - 1) vec.pop_back();
+	else vec.erase(vec.begin() + end - start, vec.end());
 	return vec;
 }
-
-double to_double(const string& str)
-{
-	stringstream ss;
-	double result;
-	ss << str;
-	ss >> result;
-	return result;
-}
-void error(string message) { cout << message << endl; exit(1); }
 // all coincident
 template <typename Type>
 bool vector_include(vector<Type> v, Type t)
@@ -42,6 +35,21 @@ bool vector_include(vector<Type> v, Type t)
 	}
 	return false;
 }
+double to_double(const string& str)
+{
+	if (str == "pi") return acos(-1);
+	if (str == "phi") return (1 + sqrt(5)) / 2;
+	if (str == "e") return M_E;
+	if (str == "-pi") return -acos(-1);
+	if (str == "-phi") return -(1 + sqrt(5)) / 2;
+	if (str == "-e") return -M_E;
+	stringstream ss;
+	double result;
+	ss << str;
+	ss >> result;
+	return result;
+}
+void error(string message) { cout << message << endl; exit(1); }
 // return the num
 template <typename Type>
 size_t vector_finds(vector<Type> v, Type t)
@@ -53,48 +61,16 @@ size_t vector_finds(vector<Type> v, Type t)
 	}
 	return num;
 }
-// return the index
-size_t first_str(string str, string find)
-{
-	if (find.length() > str.length()) return false;
-	else if (find.length() == str.length()) return str == find;
-	for (size_t i = 0; i <= str.length() - find.length(); ++i)
-	{
-		if (str.substr(i, find.length()) == find) return i;
-	}
-	return -1;
-}
-
-void replace(string& str, size_t _off, size_t _count, string to_)
-{
-	str = str.substr(0, _off) + to_ + str.substr(_off + _count + 1, str.length() - _off - _count - 1);
-}
-
-void change_all(initializer_list<char> ch_old, initializer_list<char> ch_new, string& str)
-{
-	for (size_t i = 0; i < str.length(); ++i)
-	{
-		size_t j = 0;
-		for (char ch : ch_old)
-		{
-			if (str[i] == ch)
-			{
-				str[i] = *(ch_new.begin() + j);
-				break;
-			}
-			++j;
-		}
-	}
-}
-
 bool isnum(char ch)
 {
 	if ('0' <= ch && ch <= '9') return true;
 	else if (ch == '.') return true;
 	else return false;
 }
-bool isnum(string str)
+bool isnum(string str, bool constants_ = true)
 {
+	if (str[0] == '-') return isnum(str.substr(1, str.length() - 1));
+	if (constants_ && vector_include(constants, str)) return true;
 	size_t num_of_dots = 0;
 	for (size_t i = 0; i < str.length(); ++i)
 	{
@@ -109,37 +85,73 @@ vector<string> parse(string equation)
 {
 	vector<string> stack;
 	string temp = "";
+	bool negative_sign = false;
 	for (size_t i = 0; i < equation.length(); ++i)
 	{
 		if (isnum(equation[i])) temp += equation[i];
-		else if (vector_include(empties, static_cast<string>("" + equation[i])))
+		else if (equation[i] == 'e') temp += equation[i]; // e
+		else if (isnum(equation.substr(i, 2))) temp += equation.substr(i, 2), ++i; // pi
+		else if (isnum(equation.substr(i, 3))) temp += equation.substr(i, 3), i += 2; // phi
+		else if (vector_include(empties, equation.substr(i, 1)))
 		{
 			if (temp != "")
 			{
 				stack.push_back(temp);
 				temp = "";
 			}
-			continue;
 		}
 		else if (vector_include(operators, equation.substr(i, 1)))
 		{
-			stack.push_back(temp);
+			if (equation[i] == '-')
+			{
+				if (i == 0) temp = "-";
+				else if (!isnum(stack[stack.size() - 1])) temp = "-";
+			}
+			if (temp == "-") continue;
+			if (temp != "") stack.push_back(temp);
 			stack.push_back(equation.substr(i, 1));
 			temp = "";
 		}
 		else if (vector_include(operators, equation.substr(i, 3)))
 		{
-			stack.push_back(temp);
+			if (temp != "") stack.push_back(temp);
 			stack.push_back(equation.substr(i, 3));
 			temp = "";
 			i += 2;
 		}
+		else if (vector_include(operators, equation.substr(i, 4)))
+		{
+			if (temp != "") stack.push_back(temp);
+			stack.push_back(equation.substr(i, 4));
+			temp = "";
+			i += 3;
+		}
 		else if (vector_include(operators, equation.substr(i, 5)))
 		{
-			stack.push_back(temp);
+			if (temp != "") stack.push_back(temp);
 			stack.push_back(equation.substr(i, 5));
 			temp = "";
 			i += 4;
+		}
+		else if (vector_include(constants, equation.substr(i, 1)))
+		{
+			if (temp != "") stack.push_back(temp);
+			stack.push_back(equation.substr(i, 1));
+			temp = "";
+		}
+		else if (vector_include(constants, equation.substr(i, 2)))
+		{
+			if (temp != "") stack.push_back(temp);
+			stack.push_back(equation.substr(i, 2));
+			temp = "";
+			i += 1;
+		}
+		else if (vector_include(constants, equation.substr(i, 3)))
+		{
+			if (temp != "") stack.push_back(temp);
+			stack.push_back(equation.substr(i, 3));
+			temp = "";
+			i += 2;
 		}
 		else error("Invalid charactor: " + equation[i]);
 	}
@@ -151,7 +163,7 @@ double no_brackets_calculate(vector<string> stack)
 {
 	if (stack.size() == 1 && isnum(stack[0])) return to_double(stack[0]);
 
-	// sin cos tan log log10
+	// sin asin cos acos tan atan log log10 sqr cub
 	size_t num_of_powers = 0;
 	size_t power_begin; // the index
 	size_t power_end; // the index
@@ -168,7 +180,7 @@ double no_brackets_calculate(vector<string> stack)
 					power_end = i;
 					vector<string>::iterator stackitbegin = stack.begin() + power_begin;
 					vector<string>::iterator stackitend = stack.begin() + power_end + 1;
-					double power = no_brackets_calculate(subvector(stack, power_begin, power_end));
+					double power = no_brackets_calculate(subvector(stack, power_begin + 1, power_end + 1));
 					if (power_end == stack.size() - 1) error("No number after \"" + stack[power_end - 2] + stack[power_end - 1] + stack[power_end] + "\".");
 					else if (!isnum(stack[power_end + 1])) error("No number after \"" + stack[power_end - 2] + stack[power_end - 1] + stack[power_end] + "\".");
 					string tp = stack[power_begin - 1];
@@ -180,16 +192,26 @@ double no_brackets_calculate(vector<string> stack)
 						stack[power_begin - 1] = to_string(pow(tan(to_double(stack[power_end + 1])), power));
 					else if (tp == "log")
 						stack[power_begin - 1] = to_string(pow(log(to_double(stack[power_end + 1])), power));
-					else // log10
+					else if (tp == "log10")
 						stack[power_begin - 1] = to_string(pow(log10(to_double(stack[power_end + 1])), power));
+					else if (tp == "asin")
+						stack[power_begin - 1] = to_string(pow(asin(to_double(stack[power_end + 1])), power));
+					else if (tp == "acos")
+						stack[power_begin - 1] = to_string(pow(acos(to_double(stack[power_end + 1])), power));
+					else if (tp == "atan")
+						stack[power_begin - 1] = to_string(pow(atan(to_double(stack[power_end + 1])), power));
+					else if (tp == "sqrt")
+						stack[power_begin - 1] = to_string(pow(sqrt(to_double(stack[power_end + 1])), power));
+					else if (tp == "cubr")
+						stack[power_begin - 1] = to_string(pow(to_double(stack[power_end + 1]), power / 3));
 					stack.erase(stackitbegin, stackitend + 1);
 					i = power_begin - 1;
 					in_power = false;
 				}
 			}
 		}
-		else if (stack[i] == "sin" || stack[i] == "cos" || stack[i] == "tan" ||
-			stack[i] == "log" || stack[i] == "log10")
+		else if (vector_include(operators, stack[i]) && !vector_include(binary_op, stack[i]) &&
+			stack[i] != "(" && stack[i] != ")")
 		{
 			if (i == stack.size() - 1) error("No number after \"" + stack[i] + "\".");
 			else if (stack[i + 1] == "^") // as sin^2 3.14159
@@ -197,7 +219,7 @@ double no_brackets_calculate(vector<string> stack)
 				in_power = true;
 				num_of_powers++;
 				if (num_of_powers == 1)
-					power_begin = i;
+					power_begin = i + 1;
 			}
 			else if (isnum(stack[i + 1])) // as sin 3.14159
 			{
@@ -210,8 +232,19 @@ double no_brackets_calculate(vector<string> stack)
 					stack[i] = to_string(tan(to_double(stack[i + 1])));
 				else if (tp == "log")
 					stack[i] = to_string(log(to_double(stack[i + 1])));
-				else // log10
+				else if (tp == "log10")
 					stack[i] = to_string(log10(to_double(stack[i + 1])));
+				else if (tp == "asin")
+					stack[i] = to_string(asin(to_double(stack[i + 1])));
+				else if (tp == "acos")
+					stack[i] = to_string(acos(to_double(stack[i + 1])));
+				else if (tp == "atan")
+					stack[i] = to_string(atan(to_double(stack[i + 1])));
+				else if (tp == "sqrt")
+					stack[i] = to_string(sqrt(to_double(stack[i + 1])));
+				else if (tp == "cubr")
+					stack[i] = to_string(pow(to_double(stack[i + 1]), 1.0 / 3.0));
+				stack.erase(stack.begin() + i + 1);
 				--i;
 			}
 		}
@@ -284,49 +317,7 @@ double no_brackets_calculate(vector<string> stack)
 
 double no_brackets_calculate(string equation)
 {
-	vector<string> stack = parse(equation);
-	/*for (size_t t = 0; t < equation.length(); ++t)
-	{
-		if (isnum(equation[t])) temp += equation[t];
-		else if (vector_include(operators, "" + equation[t]) ||
-			vector_include(operators, equation.substr(t, 3)) ||
-			vector_include(operators, equation.substr(t, 5)))
-		{
-			if (vector_include(binary_op, "" + equation[t]))
-			{
-				if (equation[t] == '^')
-				{
-					string next_num = "";
-					size_t j;
-					for (j = t + 1; j < equation.length(); ++j)
-					{
-						if (vector_include(binary_op, "" + equation[j])) break;
-						else if (vector_include(empties, "" + equation[j])) continue;
-						else if (isnum(equation[j])) next_num += equation[j];
-						else
-						{
-							error("Invalid charactor: " + equation[j]);
-							goto BREAK;
-						}
-					}
-					if (next_num == "")
-					{
-						error("No numbers between operator ^ and " + equation[j]);
-						break;
-					}
-				}
-				else if (equation[t] == '*' || equation[t] == '/')
-				{
-
-				}
-			}
-
-			if (vector_include(operators, equation.substr(t, 5))) t += 4;
-			else if (vector_include(operators, equation.substr(t, 3))) t += 2;
-		}
-	}
-BREAK:*/
-	return no_brackets_calculate(stack);
+	return no_brackets_calculate(parse(equation));
 }
 
 double calculate(vector<string> stack)
@@ -341,10 +332,10 @@ double calculate(vector<string> stack)
 		if (lefts < rights) error(to_string(rights - lefts) + " right bracket(s) are missing at the end.");
 		else error(to_string(lefts - rights) + " left bracket(s) are missing at the beginning.");
 	}
-	if (vector_include(stack, static_cast<string>("("))) return no_brackets_calculate(stack);
+	if (!vector_include(stack, static_cast<string>("("))) return no_brackets_calculate(stack);
 	for (size_t i = 0; i < stack.size(); ++i)
 	{
-		if (stack[i] == "(") //++layer, layer_begin = i;
+		if (stack[i] == "(")
 		{
 			++layer;
 			if (layer == 1) layer_begin = i;
@@ -366,7 +357,7 @@ double calculate(vector<string> stack)
 				double result = calculate(subvector(stack, layer_begin + 1, layer_end));
 				stack.erase(stack.begin() + layer_begin + 1, stack.begin() + layer_end + 1);
 				stack[layer_begin] = to_string(result);
-				i = layer_begin + 1;
+				i = layer_begin;
 			}
 		}
 	}
--- Calculator/main.cpp
+++ Calculator/main.cpp
@@ -1,10 +1,22 @@
 #include <iostream>
+#include <iomanip>
 #include "calculator.h"
 using namespace std;
 int main()
 {
 	string str;
 	getline(cin, str);
-	cout << calculate(str) << endl;
+	while (str != "exit")
+	{
+		double result = calculate(str);
+		cout << setprecision(14) << result << endl;
+	GET:
+		getline(cin, str);
+		if (str == "cls")
+		{
+			system("cls");
+			goto GET;
+		}
+	}
 	return 0;
 }
